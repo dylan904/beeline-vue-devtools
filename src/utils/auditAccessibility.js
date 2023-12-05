653ea64630e9d730e2242b2ae5d76eacf3b06bfc
dylan904 <tazmaxey@yahoo.com>
2023-12-05 17:36:25 -0500

    Update auditAccessibility.js

--- src/utils/auditAccessibility.js
+++ src/utils/auditAccessibility.js
@@ -30,7 +30,7 @@ function getClosestComponent(vEl) {
   }
 
 export function auditAccessibility(compEls) {
-    console.log('test')
+    console.log('test', compEls)
     const script = document.createElement("script");
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/axe-core/4.8.2/axe.min.js';
     script.type = 'text/javascript';
