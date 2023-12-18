a5714218185193d343261c7cf8c5984b1baf79f9
Dylan Maxey <dmaxey@beeline.com>
2023-12-18 00:33:06 -0500

    update devtools refs

--- src/devtools.js
+++ src/devtools.js
@@ -1,12 +1,13 @@
+
 import { ref, watch } from 'vue'
 import devtools from '@vue/devtools'
 import { setupDevtoolsPlugin } from '@vue/devtools-api'
 import auditA11Y from './utils/audit'
 import getInspectorActions from './utils/devtools/getInspectorActions'
 import getInspectorNodeActions from './utils/devtools/getInspectorNodeActions'
-import setInspectorState from './utils/devtools/setInspectorState'
-import setInspectorTree from './utils/devtools/setInspectorTree'
-import createAxeScript from './utils/audit/createAxeScript'
+import createAxeScript from 'beeline-vue-devtools/src/utils/audit/createAxeScript'
+import setInspectorState from './setInspectorState'
+import setInspectorTree from './setInspectorTree'
 
 if (import.meta.hot) {
     import.meta.hot.on('revisions-update', revisions => {
@@ -21,6 +22,7 @@ const inspectorId = 'bln-a11y'
 let devtoolsAPI
 const violatorsRef = ref([])
 let init = false
+window.violations = violationsRef.value
 
 export async function prepareA11YAudit(router) {
     if (process.env.AUDITA11Y)
@@ -32,8 +34,8 @@ export async function prepareA11YAudit(router) {
         watch(compEls, async els => {
             await auditA11Y(els, router, violationsRef.value)
             console.log('try send mytree', violationsRef.value, devtoolsAPI)
-            //if (devtoolsAPI)
-                //devtoolsAPI.sendInspectorTree(inspectorId)
+            if (devtoolsAPI)
+                devtoolsAPI.sendInspectorTree(inspectorId)
         })
     
         watch(() => router.currentRoute.value,
@@ -41,8 +43,8 @@ export async function prepareA11YAudit(router) {
                 violationsRef.value = []
                 await auditA11Y(compEls.value, router, violationsRef.value)
                 console.log('try send mytree', violationsRef.value, devtoolsAPI)
-                //if (devtoolsAPI)
-                    //devtoolsAPI.sendInspectorTree(inspectorId)
+                if (devtoolsAPI)
+                    devtoolsAPI.sendInspectorTree(inspectorId)
             }
         )
     }
@@ -58,21 +60,21 @@ export const DevtoolsPlugin = {
             devtoolsAPI = api
             
             const componentInstances = await api.getComponentInstances(app)
+            window.componentInstances = componentInstances
             const relevantComponentInstances = componentInstances.filter(instance => instance.type.__file && instance.subTree.el.nodeType === 1)
             compEls.value = relevantComponentInstances.map(instance => instance.subTree.el)
 
             api.on.getInspectorTree(async payload => {
                 if (payload.inspectorId === inspectorId && init) {
-                    violatorsRef.value = []
                     console.log('set mytree', JSON.parse(JSON.stringify(payload.rootNodes)), JSON.parse(JSON.stringify(violatorsRef.value)), JSON.parse(JSON.stringify(violationsRef.value)))
-                    await setInspectorTree(payload, api, violatorsRef.value, violationsRef.value)
+                    await setInspectorTree(payload, api, violatorsRef, violationsRef.value, componentInstances)
                     console.log('mytree was set', JSON.parse(JSON.stringify(payload.rootNodes)), JSON.parse(JSON.stringify(violatorsRef.value)), JSON.parse(JSON.stringify(violationsRef.value)))
                 }
             })
 
             api.on.getInspectorState(async payload => {
                 if (payload.inspectorId === inspectorId) {
-                    await setInspectorState(payload, api, violatorsRef.value, violationsRef.value, componentInstances)
+                    await setInspectorState(payload, api, violatorsRef.value, violationsRef.value, componentInstances, relevantComponentInstances)
                 }
             })
 
