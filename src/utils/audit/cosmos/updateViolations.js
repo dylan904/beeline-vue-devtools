73d749ab0bb4f4bc9da1f27a52c42912a363735f
Dylan Maxey <dmaxey@beeline.com>
2024-01-08 10:28:44 -0500

    debug updateViolations

--- src/utils/audit/cosmos/updateViolations.js
+++ src/utils/audit/cosmos/updateViolations.js
@@ -1,6 +1,8 @@
 export default async function updateViolations(id, ops, modified=false) {
   const container = this.getContainer(modified)
+  console.log('updateViolations', {modified, id, ops})
   const item = container.item(id)
+  console.log('updateVItem', item)
   const { resource } = await item.patch(ops)
 
   return resource
