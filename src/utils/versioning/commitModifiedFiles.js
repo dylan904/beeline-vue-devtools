e73266878fb8eaa6d71d0be333a04d2309187bfb
Dylan Maxey <dmaxey@beeline.com>
2024-01-13 11:12:50 -0500

    fileDiffersFromCommit() debug

--- src/utils/versioning/commitModifiedFiles.js
+++ src/utils/versioning/commitModifiedFiles.js
@@ -9,8 +9,15 @@ export default async function commitModifiedFiles(filePaths) {
   for (const filePath of filePaths) {
     if (await git.isFileTracked(filePath)) {
       const exisitingHash = await git.getFileCommitHash(filePath, defaultBranch)
-      const fileModified = await git.fileDiffersFromCommit(filePath, exisitingHash) // differs from default branch version of file...
-      console.log('file rev check', fileModified, {filePath, exisitingHash})
+      const fileModified = await git.fileDiffersFromCommit(filePath, exisitingHash) // differs from default branch, remote origin...
+      console.log('file rev check', {
+        filePath, 
+        exisitingHash,
+        fileModified,
+        test: await git.fileDiffersFromCommit('/Users/dylanmaxey/Downloads/temp/vue-project-5/src/components/TheWelcome.vue', '0d6874d97abedb4d9eb5a737758fb8232098101b'),
+        test2: filePath === '/Users/dylanmaxey/Downloads/temp/vue-project-5/src/components/TheWelcome.vue',
+        test3: exisitingHash === '0d6874d97abedb4d9eb5a737758fb8232098101b'
+      })
       if (fileModified) {
         // TODO: delete linked violations from pending DB (linked via violation -> node -> component -> hash)
         newCommitFiles.push(filePath)
