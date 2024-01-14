1b66ce8f1d4a61812f4916bf7cefe35c670a5dbe
Dylan Maxey <dmaxey@beeline.com>
2024-01-13 21:25:32 -0500

    batch update violations

--- src/utils/audit/cosmos/updateViolations.js
+++ src/utils/audit/cosmos/updateViolations.js
@@ -1,14 +1,22 @@
 export default async function updateViolations(id, ops, modified=false) {
   const container = this.getContainer(modified)
-  console.log('updateViolations', {modified, id, ops, container})
   const item = container.item(id)
-  console.log('updateVItem', item)
+  console.log('updateViolations', {modified, id, ops, container, item})
 
   try {
-    const { resource } = await item.patch(ops)
+    const batchSize = 10
+    const resources = []
+
+    for (let i=0; i<ops.length; i+=batchSize) {
+      const batch = ops.slice(i, i + batchSize)
+      const { resource } = await item.patch(batch)
+      resources.push(resource)
+    }
+
     console.log("updateVdone: Item patched successfully:", resource)
-    return resource
-  } catch (error) {
+    return resources
+  } 
+  catch (error) {
     if (error.code === 404) {
       console.error("Document not found. It may have been deleted or never existed.")
     } else {
