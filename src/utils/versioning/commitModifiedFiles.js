cb407f0983641c9567ce6668f27b83c309a3f8ec
Dylan Maxey <dmaxey@beeline.com>
2024-01-14 19:54:17 -0500

    log cleanup

--- src/utils/versioning/commitModifiedFiles.js
+++ src/utils/versioning/commitModifiedFiles.js
@@ -3,22 +3,20 @@ import git from './git.js'
 export default async function commitModifiedFiles(filePaths) {
   const newCommitFiles = []
   const existingCommitFiles = []
-
   const defaultBranch = await git.getDefaultBranch()
 
   for (const filePath of filePaths) {
     if (await git.isFileTracked(filePath)) {
       const exisitingHash = await git.getFileCommitHash(filePath, defaultBranch)
       const fileModified = await git.fileDiffersFromCommit(filePath, exisitingHash, 0, defaultBranch) // differs from default branch, remote origin...
-      console.log('file rev check', {
-        filePath, exisitingHash, fileModified
-      })
+
       if (fileModified) {
         // TODO: delete linked violations from pending DB (linked via violation -> node -> component -> hash)
         newCommitFiles.push(filePath)
       }
-      else 
+      else {
         existingCommitFiles.push(filePath)
+      }
     }
     else {
       newCommitFiles.push(filePath)
