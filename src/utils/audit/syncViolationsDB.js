46712c036c15cc1d70eea4900c2d55e7c3a130d3
Dylan Maxey <dmaxey@beeline.com>
2024-01-20 18:25:23 -0500

    syncViolationsDB() -> dynamic import git

--- src/utils/audit/syncViolationsDB.js
+++ src/utils/audit/syncViolationsDB.js
@@ -1,7 +1,7 @@
 import cosmos from './cosmos/index.js'
 import { appendAndProcessViolations } from './appendViolations.js'
 import syncViolation from '../versioning/syncViolation.js'
-import git from '../versioning/git.js'
+
 
 export default async function syncViolationsDB(violations, urlKey=null) {
   const currentQueryResults = await cosmos.queryViolations(urlKey, false)
@@ -13,8 +13,6 @@ export default async function syncViolationsDB(violations, urlKey=null) {
   }))
 
   for (const qResult of qResults) {
-    console.log({'qResult.current': qResult.current, 'qResult.pending': qResult.pending})
-
     const dbViolations = {
       current: qResult.current.violations || [],
       pending: qResult.pending.violations || []
@@ -47,8 +45,6 @@ export default async function syncViolationsDB(violations, urlKey=null) {
       const opsFromCurrent = await getUpdatedOps(dbViolations.current, dbViolations.pending, false)
       const opsFromPending = await getUpdatedOps(dbViolations.pending, dbViolations.current, true)
 
-      console.log({ opsFromPending, opsFromCurrent })
-
       const currentOps = [ ...opsFromPending.current, ...opsFromCurrent.current ]
       if (currentOps.length) {
         //await cosmos.updateViolations(qResultCurrent.id, currentOps)
@@ -58,15 +54,28 @@ export default async function syncViolationsDB(violations, urlKey=null) {
       if (pendingOps.length) {
         //await cosmos.updateViolations(qResultPending.id, pendingOps, true)
       }
+
+      console.log({ currentOps, pendingOps })
     }
   }
 }
+
+const fileDiffersStates = {}
+let git
+
+if (typeof window === undefined)
+  git = await import('../git.js')
 
 async function getUpdatedOps(srcViolations, violations, isPending) {
   const ops = { pending: [], current: [] }
 
   for (const [vIdx, violation] of srcViolations.entries()) {
-    syncViolation(violation, vIdx, violations, isPending, ops, async (component) => await git.fileDiffersFromCommit(component.file, component.commitHash))
+    syncViolation(violation, vIdx, violations, isPending, ops, async (component) => {
+      if (!fileDiffersStates.hasOwnProperty(component.file))
+        fileDiffersStates[component.file] = await git.fileDiffersFromCommit(component.file, component.commitHash)
+
+      return fileDiffersStates[component.file]
+    })
   }
   
   return ops
